7f74347426cc3e0ba70e312c69649d8dbffbe19d
Akshay T P <akshaytp906@gmail.com>
2026-09-12 13:02:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = "losblancos-secret-key-2026"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-fallback-key-change-me")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     db_url = os.environ.get("DATABASE_URL") or "sqlite:///database.db"
